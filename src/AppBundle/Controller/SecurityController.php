1326ede0c44f9dc7002efcc5a0eb21693ed4c193
Patrick McDougle <patrick@patrickmcdougle.com>
2015-08-15 20:56:17 -0700

    Added Remember Me functionality

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -4,16 +4,13 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\Form\FormError;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
 {
     /**
      * @Route("/login", name="login")
      */
-    public function loginAction(Request $request)
+    public function loginAction()
     {
         $form = $this->createForm('login_form');
         return $this->render('Security/login_form.html.twig', ['form' => $form->createView()]);
